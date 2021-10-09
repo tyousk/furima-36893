a2968854bc35bad00e8c8d2b6955a82bad5f1bcf
tyousk <tyou22211@gmail.com>
2021-10-10 08:08:41 +0900

    エラーメッセージの表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    validates :birth_day
+
+    with_options format: {with: /\A[ぁ-んァ-ン一-龥]/} do
+      validates :first_name
+      validates :last_name
+    end
+
+    with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+  end
 end
