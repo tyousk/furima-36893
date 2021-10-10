baa088736587ad2049d2cdb5cb653c2312533f45
tyousk <tyou22211@gmail.com>
2021-10-10 13:24:20 +0900

    単体テストコード作成

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'test'}
+    email                 {'test@example'}
+    password              {'00000a'}
+    password_confirmation {'00000a'}
+		first_name            {'テスト'}
+		last_name             {'テスト'}
+		first_name_kana       {'テスト'}
+		last_name_kana        {'テスト'}
+		birth_day             {'2000-01-01'}
+  end
+end
