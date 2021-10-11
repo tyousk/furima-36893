1418e3174f526a78e0b71c8e58213acef5e370e2
tyousk <tyou22211@gmail.com>
2021-10-12 08:45:44 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,12 @@ class ItemsController < ApplicationController
   end
 
 	def create
+		Item.create(item_params)
   end
-	
+
+  private
+
+  def message_params
+    params.require(:item).permit(:name, :about, :category_id, :statues_id, :fee_id, :prefecture_id, :send_day_id, :price, :user, :image)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ApplicationRecord
+	has_one_attached :buy_record
+	belongs_to :user
+	has_one_attached :image
 end
