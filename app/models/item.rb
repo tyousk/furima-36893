9689c0bea6073ed88aa4e286283a73bfa2424c71
tyousk <tyou22211@gmail.com>
2021-10-13 11:25:17 +0900

    Rubocop実行

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,16 @@
 class Item < ApplicationRecord
-	extend ActiveHash::Associations::ActiveRecordExtensions
-	has_one_attached :buy_record
-	belongs_to :user
-	has_one_attached :image
-	validates :name, :about, :price, :image, presence: true
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  has_one_attached :buy_record
+  belongs_to :user
+  has_one_attached :image
+  validates :name, :about, :price, :image, presence: true
   belongs_to :category
-	belongs_to :statues
-	belongs_to :fee
-	belongs_to :prefecture
-	belongs_to :send_day
+  belongs_to :statues
+  belongs_to :fee
+  belongs_to :prefecture
+  belongs_to :send_day
 
-	validates :category_id, :statues_id, :fee_id, :prefecture_id, :send_day_id, numericality: { other_than: 1 }
-	validates_inclusion_of :price, in:300..9999999
-	validates :price, numericality: true
+  validates :category_id, :statues_id, :fee_id, :prefecture_id, :send_day_id, numericality: { other_than: 1 }
+  validates_inclusion_of :price, in: 300..9_999_999
+  validates :price, numericality: true
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -13,60 +13,60 @@ RSpec.describe Item, type: :model do
     end
 
     context '商品登録がうまくいかないとき' do
-      it "「画像」が空だと登録できない" do
+      it '「画像」が空だと登録できない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-      it "「商品名」が空だと登録できない" do
+      it '「商品名」が空だと登録できない' do
         @item.name = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Name can't be blank")
       end
-      it "「商品の説明」が空だと登録できない" do
+      it '「商品の説明」が空だと登録できない' do
         @item.about = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("About can't be blank")
       end
-      it "「カテゴリー」が空だと登録できない" do
+      it '「カテゴリー」が空だと登録できない' do
         @item.category_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category must be other than 1")
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
-      it "「商品の状態」が空だと登録できない" do
+      it '「商品の状態」が空だと登録できない' do
         @item.statues_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Statues must be other than 1")
+        expect(@item.errors.full_messages).to include('Statues must be other than 1')
       end
-      it "「配送料の負担」が空だと登録できない" do
+      it '「配送料の負担」が空だと登録できない' do
         @item.fee_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Fee must be other than 1")
+        expect(@item.errors.full_messages).to include('Fee must be other than 1')
       end
-      it "「発送元の地域」が空だと登録できない" do
+      it '「発送元の地域」が空だと登録できない' do
         @item.prefecture_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+        expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
       end
-      it "「発送までの日数」が空だと登録できない" do
+      it '「発送までの日数」が空だと登録できない' do
         @item.send_day_id = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Send day must be other than 1")
+        expect(@item.errors.full_messages).to include('Send day must be other than 1')
       end
-      it "「値段」が空だと登録できない" do
+      it '「値段」が空だと登録できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it "「値段」が1~9999999でないと登録できない" do
+      it '「値段」が1~9999999でないと登録できない' do
         @item.price = '1'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not included in the list")
+        expect(@item.errors.full_messages).to include('Price is not included in the list')
       end
-      it "「値段」が半角数字でないと登録できない" do
+      it '「値段」が半角数字でないと登録できない' do
         @item.price = 'あ'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price is not a number")
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
     end
   end
